189fae062f5239a0c90ef92086c9e3f6672223ef
Tishida <archersparadox.100@gmail.com>
2024-04-09 20:47:25 +0900

    migrationのmain

--- shared/database/migration/main.go
+++ shared/database/migration/main.go
@@ -17,64 +17,71 @@ func main() {
 	config.LoadConfig()
 
 	// Initialize the database
-	db := dbInit()
-
+	tenantDBInit()
 	// Drop tables (constraints will be taken care of)
 	// db.Migrator().DropTable(&tenant.Todo{}, &tenant.AttachmentFile{})
+	// db.Migrator().DropTable(&tenant.Todo{})
 
 	// Perform migration using go-gormigrate and rollback if it fails
-	migrate(db)
+
+	// generateTableStruct(db)
 
 	// Rollback the last successful migration
 }
 
-func dbInit() *gorm.DB {
+func tenantDBInit() {
 	cfg := config.Conf
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.DbUser, cfg.DbPassword, cfg.DbHost, cfg.DbPort, cfg.DbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.DbUser, cfg.DbPassword, cfg.DbHost, cfg.DbPort, cfg.DbTenant)
 	db, err := gorm.Open(mysql.Open(dsn))
 	if err != nil {
 		fmt.Println(err)
 		panic("failed to connect database")
 	}
 
-	GenerateTableStruct(db)
-	fmt.Println("db initialized!")
-	return db
-}
+	fmt.Println("tenant db connected!")
 
-func GenerateTableStruct(db *gorm.DB) {
-	g := gen.NewGenerator(gen.Config{
-		OutPath: "query",
-		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
-	})
-
-	g.UseDB(db) // reuse your gorm db
-
-	g.ApplyBasic(
-		// Generate structs from all tables of current database
-		g.GenerateAllTable(),
-	)
-	// Generate the code
-	g.Execute()
-}
-
-func migrate(db *gorm.DB) {
-	m := gormigrate.New(db, gormigrate.DefaultOptions, []*gormigrate.Migration{
+	// migrate tenant db
+	tenantMigrations := []*gormigrate.Migration{
 		{
 			ID: "202404070000",
 			Migrate: func(tx *gorm.DB) error {
 				// it's a good pratice to copy the struct inside the function,
 				// so side effects are prevented if the original struct changes during the time
-				// tx.Migrator().CreateTable(&tenant.Todo{}, &tenant.AttachmentFile{})
-				// tx.Migrator().CreateIndex(&tenant.AttachmentFile{}, "todo_id")
-				return tx.Migrator().AutoMigrate(tenant.Todo{}, tenant.AttachmentFile{})
+				return tx.Migrator().AutoMigrate(&tenant.Todo{}, &tenant.AttachmentFile{})
 			},
 			Rollback: func(tx *gorm.DB) error {
 				log.Println("Migration Rollback")
 				return tx.Migrator().DropTable(&tenant.Todo{}, &tenant.AttachmentFile{})
 			},
 		},
+	}
+	migrate(db, tenantMigrations)
+	fmt.Println("tenant db migrated!")
+
+	generateTableStruct(db)
+	fmt.Println("tenant struct generated!")
+
+}
+
+func generateTableStruct(db *gorm.DB) {
+	g := gen.NewGenerator(gen.Config{
+		OutPath:      "shared/database",
+		Mode:         gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+		ModelPkgPath: "./tenant/model",
 	})
+
+	g.UseDB(db) // reuse your gorm db
+
+	g.ApplyBasic(
+		// Generate structs from all tables of current database
+		g.GenerateAllTable()...,
+	)
+	// Generate the code
+	g.Execute()
+}
+
+func migrate(db *gorm.DB, migrations []*gormigrate.Migration) {
+	m := gormigrate.New(db, gormigrate.DefaultOptions, migrations)
 
 	e := m.Migrate()
 	if e != nil {
