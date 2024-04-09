f402ab1e9626c14a75f27aad085cecd6287bd8d7
Tishida <archersparadox.100@gmail.com>
2024-04-09 17:10:51 +0900

    table修正

--- shared/database/migration/tenant/todo.go
+++ shared/database/migration/tenant/todo.go
@@ -9,10 +9,10 @@ import (
 type Todo struct {
 	CreatedAt       time.Time `gorm:"type:timestamp;not null;default:now()"`
 	UpdatedAt       time.Time `gorm:"type:timestamp;not null;default:now()"`
-	CreateUserId    uuid.UUID `gorm:"type:uuid;not null"`
-	UpdateUserId    uuid.UUID `gorm:"type:uuid;not null"`
-	TenantId        uuid.UUID `gorm:"type:uuid;not null"`
-	Id              uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	CreateUserId    uuid.UUID `gorm:"type:char(36);not null"`
+	UpdateUserId    uuid.UUID `gorm:"type:char(36);not null"`
+	TenantId        uuid.UUID `gorm:"type:char(36);not null;index"`
+	Id              uuid.UUID `gorm:"type:char(36);not null;primary_key;index"`
 	Title           string    `gorm:"type:varchar(255);not null"`
 	Description     string    `gorm:"type:text"`
 	IsDeleted       bool      `gorm:"type:boolean;default:false"`
@@ -22,14 +22,13 @@ type Todo struct {
 type AttachmentFile struct {
 	CreatedAt    time.Time `gorm:"type:timestamp;not null;default:now()"`
 	UpdatedAt    time.Time `gorm:"type:timestamp;not null;default:now()"`
-	CreateUserId uuid.UUID `gorm:"type:uuid;not null"`
-	UpdateUserId uuid.UUID `gorm:"type:uuid;not null"`
-	TenantId     uuid.UUID `gorm:"type:uuid;not null"`
-	Id           uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	CreateUserId uuid.UUID `gorm:"type:char(36);not null"`
+	UpdateUserId uuid.UUID `gorm:"type:char(36);not null"`
+	TenantId     uuid.UUID `gorm:"type:char(36);not null;index"`
+	Id           uuid.UUID `gorm:"type:char(36);not null;primary_key"`
 	Name         string    `gorm:"type:varchar(255);not null"`
 	Size         int64     `gorm:"type:bigint;not null"`
 	ContentType  string    `gorm:"type:varchar(255);not null"`
 	IsDeleted    bool      `gorm:"type:boolean;default:false"`
-	TodoId       uuid.UUID `gorm:"type:uuid;not null;constraint:OnDelete:CASCADE;"`
+	TodoId       uuid.UUID `gorm:"type:char(36);not null;constraint:OnDelete:CASCADE;"`
 }
-
