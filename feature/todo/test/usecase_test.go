34ea56bcbea6cf4880092cfddfa6cd970a7f219c
Tishida <archersparadox.100@gmail.com>
2024-04-15 23:21:23 +0900

    一旦CreateTestをpass

---
+++ feature/todo/test/usecase_test.go
@@ -0,0 +1,131 @@
+package todo_test
+
+import (
+	"go-todo-app/feature/todo"
+	"go-todo-app/feature/user"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+func TestTodoUsecaseImpl_Create(t *testing.T) {
+	// Given
+	testTenantId := uuid.New()
+	testUserId := uuid.New()
+	testUserContext := user.UserContext{
+		Id:        testUserId,
+		TenantId:  testTenantId,
+		Email:     "example@gmail.com",
+		LastName:  "LastName",
+		FirstName: "FirstName",
+		AccountId: uuid.MustParse("00000000-0000-0000-0000-000000000000"),
+	}
+	ctx := &gin.Context{}
+	serviceMock := &ITodoServiceMock{
+		CreateFunc: func(ctx *gin.Context, userContext user.UserContext, t *todo.Todo, session *gorm.DB) {
+			// Do nothing
+		},
+	}
+	req := todo.CreateTodoRequest{
+		Title:       "Title",
+		Description: "Description",
+	}
+	db, _ := GetNewDbMock()
+	usecase := todo.NewTodoUsecaseImpl(serviceMock, db)
+	// When
+	t.Run("正常: todo 作成", func(t *testing.T) {
+		// Then
+		usecase.Create(ctx, testUserContext, req)
+	})
+}
+
+// func TestTodoUsecaseImpl_Delete(t *testing.T) {
+// 	type fields struct {
+// 		todoService ITodoService
+// 		db          *gorm.DB
+// 	}
+// 	type args struct {
+// 		ctx         *gin.Context
+// 		userContext user.UserContext
+// 		id          uuid.UUID
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		args   args
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			tu := &TodoUsecaseImpl{
+// 				todoService: tt.fields.todoService,
+// 				db:          tt.fields.db,
+// 			}
+// 			tu.Delete(tt.args.ctx, tt.args.userContext, tt.args.id)
+// 		})
+// 	}
+// }
+
+// func TestTodoUsecaseImpl_FindAll(t *testing.T) {
+// 	type fields struct {
+// 		todoService ITodoService
+// 		db          *gorm.DB
+// 	}
+// 	type args struct {
+// 		ctx         *gin.Context
+// 		userContext user.UserContext
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		args   args
+// 		want   []Todo
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			tu := &TodoUsecaseImpl{
+// 				todoService: tt.fields.todoService,
+// 				db:          tt.fields.db,
+// 			}
+// 			if got := tu.FindAll(tt.args.ctx, tt.args.userContext); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("TodoUsecaseImpl.FindAll() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
+
+// func TestTodoUsecaseImpl_FindById(t *testing.T) {
+// 	type fields struct {
+// 		todoService ITodoService
+// 		db          *gorm.DB
+// 	}
+// 	type args struct {
+// 		ctx         *gin.Context
+// 		userContext user.UserContext
+// 		id          uuid.UUID
+// 	}
+// 	tests := []struct {
+// 		name   string
+// 		fields fields
+// 		args   args
+// 		want   Todo
+// 	}{
+// 		// TODO: Add test cases.
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			tu := &TodoUsecaseImpl{
+// 				todoService: tt.fields.todoService,
+// 				db:          tt.fields.db,
+// 			}
+// 			if got := tu.FindById(tt.args.ctx, tt.args.userContext, tt.args.id); !reflect.DeepEqual(got, tt.want) {
+// 				t.Errorf("TodoUsecaseImpl.FindById() = %v, want %v", got, tt.want)
+// 			}
+// 		})
+// 	}
+// }
